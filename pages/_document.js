c5a79ec32d5e27eb3b5558706a8c9e07dfc54049
DaffaAdityaDev <daffaaditya990@gmail.com>
2024-02-23 22:59:43 +0700

    resolve seo

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         /> */}
-        <title>Daffa Aditya Rahman</title>
+        {/* <title>Daffa Aditya Rahman</title>
         <meta name="description" content="Daffa Aditya Personal Website" />
         <meta property="og:title" content="Daffa Aditya Rahman" />
         <meta
@@ -16,8 +16,8 @@ export default function Document() {
           content="Daffa Aditya Personal Website"
         />
         <meta property="og:image" content="/image/Profile.jpg" />
-        <meta property="og:url" content="https://daffaaditya.netlify.app" />
-        <link rel="icon" href="/svg/selflogo.svg" />
+        <meta property="og:url" content="https://daffaaditya.netlify.app" /> */}
+        {/* <link rel="icon" href="/svg/selflogo.svg" /> */}
       </Head>
 
       <body className="h-screen dark:bg-slate-900 ">
