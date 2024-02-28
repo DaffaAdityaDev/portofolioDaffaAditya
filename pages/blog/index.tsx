305d33e832b2b2d8644536f2f282ee517493b60f
DaffaAdityaDev <daffaaditya990@gmail.com>
2024-02-28 20:02:08 +0700

    save

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,8 +2,8 @@ import { GraphQLClient, gql } from "graphql-request";
 import BlogCard from "../../components/Blog/BlogCard";
 import React from "react";
 import Navbar from "../../components/Navbar";
-import { getSortedPostsData } from '../../lib/posts'
-import Link from 'next/link'
+import { getSortedPostsData } from "../../lib/posts";
+import Link from "next/link";
 import { IBlogProps } from "../../components/types";
 
 export async function getStaticProps() {
@@ -16,15 +16,15 @@ export async function getStaticProps() {
 }
 const Blog = ({ allPostsData }: any) => {
   // console.log(allPostsData);
-  const allPostsData1 = []
+  const allPostsData1 = [];
 
   for (let i = 0; i < 4; i++) {
-    allPostsData1.push(allPostsData[0])
+    allPostsData1.push(allPostsData[0]);
   }
   // console.log(allPostsData1);
   return (
     <div className="grid grid-cols-12 text-white">
-      <div className="col-start-2 col-span-10">
+      <div className="col-span-10 col-start-2">
         <Navbar />
       </div>
       <div className="relative flex flex-col justify-center items-center w-full col-start-2 col-span-10 my-8">
@@ -37,20 +37,35 @@ const Blog = ({ allPostsData }: any) => {
           <p className="font-thin">Stay Informed with our featured Posts</p>
         </div>
       </div>
-      <div className="col-start-2 col-span-10">
-        <div className="grid grid-cols-2 sm:grid-cols-1 gap-6">
-          {allPostsData.map(({ id, image, date, title, timeToRead, description } : IBlogProps) => (
-            <div key={id}>
-              <BlogCard
-                id={id}
-                image={image}
-                title={title}
-                description={description}
-                date={date}
-                timeToRead={timeToRead}
-              />
-            </div>
-          ))}
+      {/* <div className="col-start-0 col-span-12">
+        <div className="relative grid h-20 grid-cols-12 py-20">
+          <div className="col-start-0 absolute -z-10 col-span-12 h-full w-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  " />
+          <p className="col-start-2">Blog Posts</p>
+        </div>
+      </div> */}
+      <div className="col-span-10 col-start-2">
+        <div className="grid grid-cols-2 gap-6 sm:grid-cols-1">
+          {allPostsData.map(
+            ({
+              id,
+              image,
+              date,
+              title,
+              timeToRead,
+              description,
+            }: IBlogProps) => (
+              <div key={id}>
+                <BlogCard
+                  id={id}
+                  image={image}
+                  title={title}
+                  description={description}
+                  date={date}
+                  timeToRead={timeToRead}
+                />
+              </div>
+            ),
+          )}
         </div>
       </div>
     </div>
